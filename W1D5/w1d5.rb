18e45440462a428b39de162ab4167588e0207d5a
Joana Beshaj <joana.beshaj.work@gmail.com>
2017-05-05 05:47:38 -0400

    Added the Queue class into the file

--- W1D5/w1d5.rb
+++ W1D5/w1d5.rb
@@ -18,10 +18,39 @@ class Stack
   end
 end
 
+puts "Tests for the Stack class"
 my_stack = Stack.new
 p my_stack.add(2)
 p my_stack.add(4)
 p my_stack.add(6)
-puts "\nJust removed the last element of the stack; it was #{my_stack.remove}."
-
+p my_stack.remove
 p my_stack.show
+
+
+class Queue
+  attr_accessor :queue_ary
+  def initialize
+    @queue_ary = []
+  end
+  
+  def enqueue(el)
+    @queue_ary.push(el)
+  end
+  
+  def dequeue
+    @queue_ary.shift
+  end
+  
+  def show
+    @queue_ary
+  end
+end
+
+puts "\n\nTests for the Queue class"
+my_queue = Queue.new
+p my_queue.enqueue("Steve")
+p my_queue.enqueue("Warren")
+p my_queue.enqueue("Bill")
+
+p my_queue.dequeue
+p my_queue.show
